7688ed2219685f4f3dd1ac37a6f99ee539edaec8
Jesus Castro <castrojesus39@gmail.com>
2023-09-19 20:06:09 -0700

    added the necessary JSX for title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,13 @@
 
 
-export function Title() {
+export function Title(props) {
     return (
-        <></>
+        <div>
+            <div className={"expense-item__title"}></div>
+            <div className={"expense-item__description"}>
+                <h2>{props.title}</h2>
+                <div className={"expense-item__price"}>{props.amount}</div>
+            </div>
+        </div>
     )
 }
